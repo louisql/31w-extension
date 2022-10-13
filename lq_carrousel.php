180f7365a6df02ee37d0bc320352440399679210
louisql <102247814+louisql@users.noreply.github.com>
2022-10-13 12:07:01 -0400

    debogage carrousel

---
+++ lq_carrousel.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ *  package Carrousel
+ * version 1.0.0 
+ */
+
+ /*
+ * Plugin name: LQ_carrousel
+ * Version 1.0.0
+ */
+
+/* filemtime()  // retourne en milliseconde le temps de la dernière sauvegarde
+plugin_dir_path() // retourne le chemin du répertoire du plugin
+__FILE__ // le fichier en train de s'exécuter
+wp_enqueue_style() // Intègre le link:css dans la page
+wp_enqueue_script() // intègre le script dans la page
+wp_enqueue_scripts // le hook */
+
+function lqc_enqueue(){
+    $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
+    $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+
+    wp_enqueue_style("lqc_style_carrousel",
+                        plugin_dir_url(__FILE__)."style.css",
+                        array(),
+                        $version_css,
+                        false);
+    wp_enqueue_script("lqc_js_carrousel",
+                        plugin_dir_url(__FILE__)."js/carrousel.js",
+                        array(),
+                        $version_js,
+                        false);
+}
+
+add_action('wp_enqueue_scripts', 'lqc_enqueue');
+
+
+ function genere_boite(){
+    $contenu = "
+
+        <div class='carrousel'>Carrousel</div>";
+    
+    return $contenu;
+ }
+ add_shortcode('lq_carrousel', 'genere_boite');
+
+// function lqc_enqueue()
+// {
+//     $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
+//     $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+//     wp_enqueue_style(
+//         "lqc_style_carrousel",
+//         plugin_dir_url(__FILE__) . "style.css",
+//         array(),
+//         $version_css,
+//         false
+//     );
+
+//     wp_enqueue_script(
+//         "lqc_js_carrousel",
+//         plugin_dir_url(__FILE__) . "js/carrousel.js",
+//         array(),
+//         $version_js,
+//         false
+//     );
+// }
+
+// add_action('wp_enqueue_scripts', 'lqc_enqueue');
+
+// function genere_boite()
+// {
+//     $contenu = "
+//                 <div class='carrousel'>Carrousel</div>
+//                 ";
+//     return $contenu;
+// }
+
+// add_shortcode('lq_carrousel', 'genere_boite');
