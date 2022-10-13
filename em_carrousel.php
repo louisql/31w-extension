84fc62c81cef450f57302963f42023253cc30b87
louisql <102247814+louisql@users.noreply.github.com>
2022-10-13 11:14:24 -0400

    C9 - ajout de css dans <style>

--- em_carrousel.php
+++ em_carrousel.php
@@ -10,7 +10,17 @@
  */
 
  function genere_boite(){
-    $contenu = "<div class='carrousel'>Carrousel</div>";
+    $contenu = "
+        <style>
+        .carrousel {
+            border: 2px solid #aaa;
+            background-color: #0f0;
+            width: 200px;
+            height: 200px;
+        }
+        </style>
+        <div class='carrousel'>Carrousel</div>";
+    
     return $contenu;
  }
  add_shortcode('lq_carrousel', 'genere_boite');
