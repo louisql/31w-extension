c7ee77f056aa144364a6db72ff757d1cf41ed7a3
louisql <102247814+louisql@users.noreply.github.com>
2022-10-14 14:29:08 -0400

    modification du style

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,3 +1,15 @@
 (function(){
     console.log("carrousel")
+    let elmBtnModale = document.querySelector(".btn_modale");
+    let elmBtnModaleFermer = document.querySelector(".btn_fermer");
+    let elmCarrousel = document.querySelector(".carrousel");
+    elmBtnModale.addEventListener('mousedown', function(){
+        console.log("bouton boite modale")
+        elmCarrousel.classList.add("carrousel--ouvrir")
+    })
+    
+    elmBtnModaleFermer.addEventListener('mousedown', function(){
+        console.log("bouton boite modale")
+        elmCarrousel.classList.remove("carrousel--ouvrir")
+    })
 })()
