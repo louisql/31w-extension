59789545305b3edfda0d0cf9e5231656a8b5976d
louisql <102247814+louisql@users.noreply.github.com>
2022-10-18 15:16:19 -0400

    gestion de la galerie

--- js/carrousel.js
+++ js/carrousel.js
@@ -9,6 +9,19 @@
     let elmBtnModale = document.querySelector(".btn_modale");
     /* Bouton temporaire pour ouvrir le carrousel */
     let elmBtnModaleFermer = document.querySelector(".btn_fermer");
+
+    /* figure qui contiendra l'ensemble  des images du carrousel */
+    let elmCarrousel__figure = document.querySelector('.carrousel__figure');
+
+    /* Le formulaire qui contiendra l'ensemble des boutons radio*/
+    let elmCarrousel__form = document.querySelector('.carrousel__form');
+
+    /*---------------------------------------- Les éléments de la galerie */
+        /*Le conteneur principal de la galerie */
+    let elmGalerie = document.querySelector(".galerie")
+
+
+
     elmBtnModale.addEventListener('mousedown', function(){
         console.log("bouton boite modale")
         elmCarrousel.classList.add("carrousel--ouvrir")
